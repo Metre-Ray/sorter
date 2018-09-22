42fe67e4ba2e28a9231aafbf70e1f8b96b6af04d
Metre-Ray <dgary@tut.by>
2018-09-22 13:30:04 +0300

    modified index

--- src/index.js
+++ src/index.js
@@ -1,30 +1,45 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.data = [];
+    this.comparator = function (a, b) {
+      if (a > b) return 1;
+      if (a <= b) return -1;
+    }
   }
 
   add(element) {
-    // your implementation
+    this.data.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.data[index];
   }
 
   get length() {
-    // your implementation
+    return this.data.length;
   }
 
   toArray() {
-    // your implementation
+    return this.data;
   }
 
   sort(indices) {
-    // your implementation
+    indices.sort(function (a, b) {
+      if (a > b) return 1;
+      if (a <= b) return -1;
+    })
+    var length = indices.length;
+    for (var j = 0; j < length - 1; j++) {
+      for (var i = 0; i < length - 1 - j; i++) {
+        if (this.comparator(this.data[indices[i]], this.data[indices[i + 1]]) > 0) {
+          [this.data[indices[i]], this.data[indices[i + 1]]] = [this.data[indices[i + 1]], this.data[indices[i]]];
+        }
+      }
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;
   }
 }
 
